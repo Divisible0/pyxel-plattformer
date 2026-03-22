b5d9ba9744ec8ff0f86e534c7a6faea53ab1acdb
--global <--global>
2026-03-22 17:34:09 +0100

    Code Cleanup

--- main.py
+++ main.py
@@ -10,48 +10,43 @@ VIEW_WIDTH = 360
 VIEW_HEIGHT = 240
 
 
-def clamp(value, minimum, maximum): #Used to prevent any value going over their maximum. Later used to prevent camera from exiting the world
+def clamp(value, minimum, maximum): # Prevents camera from clipping
     return max(minimum, min(value, maximum))
 
-# AABB collision check
-def aabb(ax, ay, aw, ah, bx, by, bw, bh): 
+def collision(ax, ay, aw, ah, bx, by, bw, bh): 
     return (
         ax < bx + bw and ax + aw > bx and
         ay < by + bh and ay + ah > by
     )
-#Player class. Contains all the player properties
+
 class Player:
     def __init__(self):
-        self.x = 120 #Starting Position
+        self.x = 120
         self.y = 704
 
-        self.w = 16 #Width
-        self.h = 16 #Height
+        self.w = 16
+        self.h = 16
 
-        # Hitbox width/height
+        # Hitbox width/height & position (centered)
         self.hitbox_w = 12
         self.hitbox_h = 15
-
-        #Hitbox position (centers the hitbox)
         self.hitbox_offset_x = 2 
         self.hitbox_offset_y = 1
 
-        self.vx = 0 #Starting Velocity
+        self.vx = 0
         self.vy = 0
-        self.gravity = 0.8 #Gravity
-        self.accel_ground = 0.7 #Acceleration on the floor
-        self.accel_air = 0.2 #Acceleration in the air
+        self.gravity = 0.8
+        self.accel_ground = 0.7
+        self.accel_air = 0.3
         self.friction_air = 0.9
         self.friction_ground = 0.01
-        self.max_fall = 8 #Prevents clipping
-        self.max_speed = 4 #Sets Max Speed
-        self.on_ground = False #Statement gets changed when on a plattform
-        self.state = "idle" #State used for movement models
-        self.air = False #State used for movement models
+        self.max_fall = 8
+        self.max_speed = 4
+        self.on_ground = False
+        self.air = False
+        self.state = "idle"
 
-        self.hp = 20 #Hp
-
-    def hitbox(self): #uses the hitbox properties from __init__ to calculate the hitbox
+    def hitbox(self):
         return (
             self.x + self.hitbox_offset_x,
             self.y + self.hitbox_offset_y,
@@ -60,39 +55,38 @@ class Player:
     )
 
     def update(self):
-        if self.vx > self.max_speed: #speed limit (positive x)
+        if self.vx > self.max_speed:
             self.vx = self.max_speed 
-        if self.vx < -self.max_speed: #speed limit (negative x)
+        if self.vx < -self.max_speed:
             self.vx = -self.max_speed 
         
-        # When in the air/on the ground uses the apropriate acceleration property
         if self.on_ground:
             accel = self.accel_ground
-            self.air = False #Updates the state of the movement model
+            friction = self.friction_ground
+            self.air = False
         else:
             accel = self.accel_air
-            self.air = True #^^^
+            friction = self.friction_air
+            self.air = True
 
-        #Sets movement
         if self.on_ground and pyxel.btnp(pyxel.KEY_SPACE):
-            self.vy = -9 #Jump
+            self.vy = -9
         if pyxel.btn(pyxel.KEY_D):
             if self.on_ground and self.vx < 0:
                 self.vx = 0
-            self.vx += accel #Uses the acceleration property to move
-            self.state = "facing_right" #Movement model state
+            self.vx += accel
+            self.state = "facing_right"
         elif pyxel.btn(pyxel.KEY_A):
             if self.on_ground and self.vx > 0:
                 self.vx = 0
-            self.vx -= accel #Uses the acceleration property to move
-            self.state ="facing_left" #Movement Model state
+            self.vx -= accel
+            self.state ="facing_left"
         else:
-            # Slow down (friction)
-            self.vx *= (self.friction_ground if self.on_ground else self.friction_air)
+            self.vx *= (friction)
             if abs(self.vx) < 0.05:
                 self.vx = 0
 
-        #Selects the correct movement model depending on the state
+        # Movement Model
         global u, v
         u, v = 0, 0
         if self.state == "idle" and self.air == False:
@@ -110,20 +104,19 @@ class Player:
         else:
             u, v = 0, 0
 
-        
-        # Saves current Position in case of colision
-        old_x = self.x
-        old_y = self.y
 
         self.vy += self.gravity
-        if self.vy > self.max_fall:  # Limits fall speed (clipping)
+        if self.vy > self.max_fall:
             self.vy = self.max_fall
 
-        #Apply Movement
+        old_x = self.x
+        old_y = self.y
+        old_hx = old_x + self.hitbox_offset_x
+        old_hy = old_y + self.hitbox_offset_y
+
         self.x += self.vx
         self.y += self.vy
 
-        # Will be set to True again when landing on something
         self.on_ground = False
 
         # World floor (fallback)
@@ -132,15 +125,11 @@ class Player:
             self.vy = 0
             self.on_ground = True
 
-        hx, hy, hw, hh = self.hitbox() #Assigns the hitbox values for the collision calculation
-
-        #saves position before collision (including hitbox properties)
-        old_hx = old_x + self.hitbox_offset_x 
-        old_hy = old_y + self.hitbox_offset_y
+        hx, hy, hw, hh = self.hitbox()
 
         # Platform collisions
         for p in platforms:
-            if aabb(hx, hy, hw, hh, p.x, p.y, p.w, p.h):
+            if collision(hx, hy, hw, hh, p.x, p.y, p.w, p.h):
 
                 # Coming from above (landing)
                 if old_hy + hh <= p.y and hy + hh > p.y:
@@ -176,12 +165,9 @@ class Platform:
 
     def draw(self):
         tiles = max(1, self.w // 16) #Calculates the width of the plattform into whole blocks (pixel/16)
-        for i in range(tiles): #repeats the process that many times
-            pyxel.blt(self.x + i * 16, self.y, 0, 16, 0, 16, 16, 0) #builds the squares next to eachother
+        for i in range(tiles):
+            pyxel.blt(self.x + i * 16, self.y, 0, 16, 0, 16, 16, 0)
 
-
-#sets the upper left edge of the camera (width is set in the top of the code)
-#this point is later moved to match the player position
 camera_x = 0
 camera_y = 0
 
@@ -194,7 +180,6 @@ def update_camera(): #Function to update camera
     camera_x = clamp(target_x, 0, WORLD_WIDTH - VIEW_WIDTH) # Prevents camera from clipping out of the world (x)
     camera_y = clamp(target_y, 0, WORLD_HEIGHT - VIEW_HEIGHT) # Prevents camera from clipping out of the world (y)
 
-#Assign variables/lists to the classes (to draw them)
 player = Player()
 platforms = [
     Platform(512, 688, 64, 16),
@@ -204,17 +189,18 @@ platforms = [
     Platform(800, 578, 64, 16)
     ]
 
-#The draw function repeats itself every frame
+def update():
+    player.update()
+    update_camera()
+
 def draw():
-    update_camera() #Camera Position
     pyxel.cls(2) #Clears Screen
-    pyxel.colors[1] = 0x000000 #Reassign colors (black)
     pyxel.camera(camera_x, camera_y) #Camera Position (set in update_camera)
     player.draw() #Draws player
     for p in platforms: #Draws all the plattforms
         p.draw()
-    pyxel.camera(0, 0)
 
 pyxel.init(VIEW_WIDTH, VIEW_HEIGHT, display_scale=3) #Smaller view = zoomed camera
+pyxel.colors[1] = 0x000000 #Reassign colors (black)
 pyxel.load("assets/resources.pyxres")
-pyxel.run(player.update, draw) #Executes the draw function & updates (every frame)
+pyxel.run(update, draw) #Executes the draw function & updates (every frame)
